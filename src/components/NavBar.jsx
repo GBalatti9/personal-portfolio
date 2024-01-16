a3e34d866a7779d91db6f73d0d9d288365d2d30b
GBalatti9 <gas.balatti@gmail.com>
2024-01-16 11:23:21 -0300

    NavBar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,26 @@
-import { AppBar, Box, Link, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Box, CardActions, Drawer, IconButton, Link, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react'
 import { ThemeToggleBtn } from './ThemeToggleBtn';
 
 const navItems = ['Home', 'About', 'Contact'];
 
 export const NavBar = () => {
+
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setIsDrawerOpen(true);
+    };
+    
+    const handleDrawerClose = () => {
+        setIsDrawerOpen(false);
+    };
+
     return (
-        <Box color="primary" sx={{ boxShadow: '1px 1px 1px rgba(secondary, 0.5)' }}>
-            <List sx={{ display: 'flex' }}>
+        <Box color="primary">
+            <List sx={{ display: { xs: 'none', sm: 'flex' } }}>
                     {
                     navItems.map(( item ) => (
                         <ListItem key={ item } disablePadding sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -25,6 +38,32 @@ export const NavBar = () => {
                     }
                 <ThemeToggleBtn />
                 </List>
+
+                <Box sx={{ display: {xs: 'flex', sm: 'none' }, justifyContent: 'space-between', px: 1 }}>
+                    <ThemeToggleBtn />
+                    <IconButton onClick={ handleDrawerOpen }>
+                        <MenuIcon/>
+                    </IconButton>
+
+                    <Drawer anchor="right" open={isDrawerOpen} sx={{ opacity: '0.9' }}>
+                        <Box sx={{ width: '100vw', height: '100%', backgroundColor: 'black' }}>
+                            <CardActions sx={{ justifyContent: 'flex-end' }}>
+                                <IconButton onClick={handleDrawerClose} >
+                                    <CloseIcon sx={{ fontSize: '3rem' }}/>
+                                </IconButton>
+                            </CardActions>
+                            <List sx={{ display: 'flex', flexDirection: 'column', height: '70%', justifyContent: 'space-around' }}>
+                                {navItems.map((item) => (
+                                    <ListItem key={item} onClick={ handleDrawerClose } sx={{ textAlign: 'center' }}>
+                                        <ListItemText
+                                        primaryTypographyProps={{fontSize: '40px'}} 
+                                        >{item}</ListItemText>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Box>
+                    </Drawer>
+                </Box>
         </Box>
     )
 }
