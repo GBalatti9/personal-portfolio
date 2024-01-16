af8eb6253e5562351c72f92cad68d005c913e8db
GBalatti9 <gas.balatti@gmail.com>
2024-01-16 09:39:57 -0300

    Palette configuration

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,8 +1,31 @@
-import { createTheme } from "@mui/material"
+import { createTheme } from "@mui/material";
+import { teal, orange, grey } from "@mui/material/colors";
 
 export const themeCreator = ( mode ) => {
     return createTheme({
         palette: {
             mode,
+            ...( mode === 'light'
+                ? {
+                    primary: teal,
+                    secondary: orange,
+                    text: {
+                        primary: grey[900],
+                        secondary: grey[800],
+                    },
+                }
+                : {
+                    primary: orange,
+                    secondary: teal,
+                    background: {
+                        default: '#121212', 
+                        paper: '#1E1E1E', 
+                    },
+                    text: {
+                        primary: '#fff', 
+                        secondary: grey[500],
+                    },
+                }),
         },
-    })}
+    });
+};
