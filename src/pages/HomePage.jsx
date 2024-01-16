80fce657fe22754ebfed6c9f503e49bd81b518af
GBalatti9 <gas.balatti@gmail.com>
2024-01-16 13:12:10 -0300

    First part of the portfolio done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, IconButton, Typography } from "@mui/material";
+import { Avatar, Box, IconButton, Link, Typography } from "@mui/material";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -7,7 +7,12 @@ import { Typewriter } from "react-simple-typewriter";
 
 export const HomePage = () => {
 
-    
+    const icons = [ 
+    { icon: <GitHubIcon />, link: 'https://github.com/GBalatti9' }, 
+    { icon: <LinkedInIcon />, link: 'https://www.linkedin.com/in/gast%C3%B3n-balatti-8b5b441ba/' }, 
+    { icon: <TwitterIcon />, link: 'https://twitter.com/Gaston_Balatti'},
+    { icon: <InstagramIcon />, link: 'https://www.instagram.com/gaston.balatti?igsh=Mjdpa3N0MDdrMHNn&utm_source=qr' }  
+]
 
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
@@ -15,23 +20,26 @@ export const HomePage = () => {
                 alt="Gaston Balatti"
                 src="/GastonCvImage.jpg"
                 sx={{ width: 200, height: 200 }} />
-            <Box>
-                <Box sx={{ ml: 2 }}>
-                    <Typography variant="h3">Gaston Balatti</Typography>
-                    <Typography variant="h5" color='primary'> 
-                            <Typewriter words={['Full-stack', 'Front-end', 'Back-end']} cursor loop={5} />
-                        <span style={{ color: 'white' }}>
-                            Dev
-                        </span>
-                        </Typography>
-                </Box>
+            <Box sx={{ ml: 2 }}>
                 <Box>
-                    <IconButton>
-                        <GitHubIcon />
-                        <LinkedInIcon />
-                        <TwitterIcon />
-                        <InstagramIcon />
-                    </IconButton>
+                    <Typography variant="h2"> Gaston Balatti</Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                        <Typography variant="h4" color="primary">
+                            <Typewriter words={['Full-stack', 'Front-end', 'Back-end']} cursor loop={5}/>
+                        </Typography>
+                        <Typography variant="h4">
+                            Dev
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box sx={{ width: '50%', display: 'flex', justifyContent: 'space-around', alignItems: 'center', mx: 'auto', mt: 1 }}>
+                    {
+                        icons.map(( icon, i ) => (
+                            <Link key={ i } color="inherit" href={ icon.link } target="_blank" sx={{ '&:hover': { color: 'orange' } }} >
+                                { icon.icon }
+                            </Link>
+                        ))
+                    }
                 </Box>
             </Box>
         </Box>
