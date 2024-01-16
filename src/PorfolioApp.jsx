3775afb3a8c360b55caa3b011ce669915d730b49
GBalatti9 <gas.balatti@gmail.com>
2024-01-16 18:31:34 -0300

    ParticlesBackground working correctly

--- src/PorfolioApp.jsx
+++ src/PorfolioApp.jsx
@@ -2,12 +2,14 @@
 import { Box } from "@mui/material";
 import { NavBar, ScrollToTopButton } from "./components";
 import { AboutPage, HomePage } from "./pages";
+import { ParticlesBackground } from "./particles/ParticlesBackground";
 
 
 export const PorfolioApp = () => {
 
     return (
         <>
+            <ParticlesBackground />
             <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
                 <ScrollToTopButton />
                 <NavBar />
---
+++ src/particles/ParticlesBackground.jsx
@@ -0,0 +1,112 @@
+import { useTheme } from "@emotion/react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+
+
+export const ParticlesBackground = () => {
+
+    const theme = useTheme();
+    console.log(theme.palette.primary.main);
+
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    const options = useMemo(
+        () => ({
+            fullScreen: {
+                enable: true,
+                zIndex: -1
+            },
+            background: {
+                color: {
+                    value: theme.palette.primary.main === "#ff9800" ? "black" : "white",
+                },
+            },
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                },
+                modes: {
+                    push: {
+                        quantity: 4,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: theme.palette.primary.main === "#ff9800" ? "#ffffff" : "#000000",
+                },
+                links: {
+                    color: theme.palette.primary.main === "#ff9800" ? "#ffffff" : "#000000",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                        default: "bounce",
+                    },
+                    random: false,
+                    speed: 1,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: 0.9,
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 1, max: 5 },
+                },
+            },
+            detectRetina: true,
+        }),
+        [theme],
+    );
+
+    if (init) {
+        return (
+            <Particles
+                id="tsparticles"
+                particlesLoaded = {particlesLoaded}
+                options={options}
+            />
+        );
+    }
+
+    return <></>;
+}
