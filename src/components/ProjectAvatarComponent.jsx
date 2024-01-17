e480255aa0f135f453eaee1ad751b366d19baf96
GBalatti9 <gas.balatti@gmail.com>
2024-01-17 11:48:41 -0300

    ProjectAvatar modularized

---
+++ src/components/ProjectAvatarComponent.jsx
@@ -0,0 +1,19 @@
+import { useTheme } from "@emotion/react";
+import { Box, Link, Typography } from "@mui/material"
+
+
+export const ProjectAvatarComponent = ({ link, category, img, title }) => {
+    const theme = useTheme();
+    console.log(theme.palette.primary.main);
+    return (
+        <Link color="inherit" sx={{ textDecoration: 'none' }} href={ link } target="_blank">
+                <Box sx={{ backgroundColor: `${ theme.palette.primary.main === '#ff9800' ? 'rgba(0, 0, 0, 0.8)' : 'rgba(201, 197, 199, 0.8)' }`, p: 2, borderRadius: '5px', '&:hover': { boxShadow: `0px 0px 2px ${theme.palette.primary.main}`}, mb: 2, width: { xs: '80%', sm: '100%' }, mx: 'auto' }} >
+                <Typography variant="h5" sx={{ textAlign: 'center' }}>{ title }</Typography>
+                <Typography variant="h6" sx={{ mb: 2, textAlign: 'center', color: theme.palette.primary.main }}>{ category }</Typography>
+                <Box sx={{ width: '200px', mx: 'auto' }}>
+                    <img src={ img } alt="" style={{ width: '100%', height: '100%' }} />
+                </Box>
+            </Box>
+        </Link>
+    )
+}
--- src/components/ProjectsAvatar.jsx
+++ src/components/ProjectsAvatar.jsx
@@ -1,5 +1,6 @@
 import { useTheme } from "@emotion/react"
 import { Box, Link, Typography } from "@mui/material"
+import { ProjectAvatarComponent } from "./ProjectAvatarComponent";
 
 const smallProjects = [
     { id: 1, title: 'Band Home Page', category: 'Front-end project', img: '/projects/TragoAmargo.png', link: 'https://trago-amargo.onrender.com/' },
@@ -9,22 +10,11 @@ const smallProjects = [
 
 export const ProjectsAvatar = () => {
 
-    const theme = useTheme();
-    console.log(theme.palette.primary.main);
-
     return (
         <Box sx={{ display: { sm: 'flex' }, justifyContent: 'space-around', }}>
             {
                 smallProjects.map(( project ) => (
-                    <Link color="inherit" sx={{ textDecoration: 'none' }} href={ project.link } target="_blank">
-                    <Box sx={{ backgroundColor: `${ theme.palette.primary.main === '#ff9800' ? 'rgba(0, 0, 0, 0.8)' : 'rgba(201, 197, 199, 0.8)' }`, p: 2, borderRadius: '5px', '&:hover': { boxShadow: `0px 0px 2px ${theme.palette.primary.main}`}, mb: 2, width: { xs: '80%', sm: '100%' }, mx: 'auto' }} >
-                        <Typography variant="h5" sx={{ textAlign: 'center' }}>{ project.title }</Typography>
-                        <Typography variant="h6" sx={{ mb: 2, textAlign: 'center', color: theme.palette.primary.main }}>{ project.category }</Typography>
-                        <Box sx={{ width: '200px', mx: 'auto' }}>
-                            <img src={project.img} alt="" style={{ width: '100%', height: '100%' }} />
-                        </Box>
-                    </Box>
-                    </Link>
+                    <ProjectAvatarComponent key={ project.id } { ...project } />
                 ))
             }
         </Box>
