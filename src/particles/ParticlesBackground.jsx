f9b8876694db95be604abedd902b6cd9d33ad8ba
GBalatti9 <gas.balatti@gmail.com>
2024-01-17 19:54:12 -0300

    More particles in the background

--- src/particles/ParticlesBackground.jsx
+++ src/particles/ParticlesBackground.jsx
@@ -80,7 +80,7 @@ export const ParticlesBackground = () => {
                     density: {
                         enable: true,
                     },
-                    value: 80,
+                    value: 200,
                 },
                 opacity: {
                     value: 0.9,
@@ -89,7 +89,7 @@ export const ParticlesBackground = () => {
                     type: "circle",
                 },
                 size: {
-                    value: { min: 1, max: 5 },
+                    value: { min: 1, max: 3 },
                 },
             },
             detectRetina: true,
